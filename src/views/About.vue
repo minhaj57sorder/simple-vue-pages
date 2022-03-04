8c64a1ea20e252b6628a52aae620087a7267abce
minhaj57sorder <74466041+minhaj57sorder@users.noreply.github.com>
2022-03-04 18:39:53 +0600

    about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,34 @@
 <template>
-    <div class="ab-sec-container">
-        <h3>About page</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste vitae! Ducimus nobis nisi officia laudantium laborum. Voluptas suscipit, voluptatum necessitatibus perspiciatis tempora unde quibusdam, quis aliquid enim illo explicabo.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste vitae! Ducimus nobis nisi officia laudantium laborum. Voluptas suscipit, voluptatum necessitatibus perspiciatis tempora unde quibusdam, quis aliquid enim illo explicabo.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste vitae! Ducimus nobis nisi officia laudantium laborum. Voluptas suscipit, voluptatum necessitatibus perspiciatis tempora unde quibusdam, quis aliquid enim illo explicabo.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste vitae! Ducimus nobis nisi officia laudantium laborum. Voluptas suscipit, voluptatum necessitatibus perspiciatis tempora unde quibusdam, quis aliquid enim illo explicabo.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste vitae! Ducimus nobis nisi officia laudantium laborum. Voluptas suscipit, voluptatum necessitatibus perspiciatis tempora unde quibusdam, quis aliquid enim illo explicabo.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste vitae! Ducimus nobis nisi officia laudantium laborum. Voluptas suscipit, voluptatum necessitatibus perspiciatis tempora unde quibusdam, quis aliquid enim illo explicabo.</p>
-    </div>
+  <div class="ab-sec-container">
+    <h3>About page</h3>
+    <p>
+      Prolific, full stack web developer with a passion for metrics and beating
+      former "best-yets." Prototyped 25 new product features per year for
+      Flexor, Inc. Decreased rework by 22% and costs by 15%. Consistently
+      receive high user experience scores for all web development projects,
+      including a 55% increase for Flexor, Inc. Passionate about building world
+      class web applications. One of my sites received a 2015 Webby for Best
+      Navigation and Structure.
+    </p>
+    <p>
+      Hard-working web developer with a flair for creating elegant solutions in
+      the least amount of time. Developed an ecommerce webapp, customer web
+      portal, documentary launch website, and donations webapp for a local
+      charity. Passionate about software architecture and cloud computing.
+      Regular attendee of web developer meetups and hackathons.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'About'
+  name: 'About',
 }
 </script>
 
 <style scoped>
-.ab-sec-container{
-    padding-left:5%;
-    padding-right:5%;
+.ab-sec-container {
+  padding-left: 5%;
+  padding-right: 5%;
 }
 </style>
